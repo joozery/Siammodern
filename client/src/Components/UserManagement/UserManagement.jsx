eb755b44536ab796b63258c61651ccbaa6a558f3
Dev Juu <devjuu@MacBook-Pro-khxng-Dev.local>
2024-11-15 16:06:16 +0700

    Initial commit

---
+++ client/src/Components/UserManagement/UserManagement.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const UserManagement = () => {
+  return (
+    <div className="userManagement">
+      <h1>การจัดการผู้ใช้งาน</h1>
+      <p>นี่คือหน้าการจัดการผู้ใช้งาน</p>
+    </div>
+  );
+};
+
+export default UserManagement;
+
